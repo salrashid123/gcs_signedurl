c0061fa489f3adb60a306f645e614f06754109a6
salrashid123 <salrashid123@gmail.com>
2021-10-18 07:21:25 -0400

    fix go sample

--- golang/server.go
+++ golang/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
 	"fmt"
 	"net/http"
 	"os"
@@ -10,9 +9,6 @@ import (
 
 	"cloud.google.com/go/storage"
 	"golang.org/x/net/http2"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/iamcredentials/v1"
 )
 
 var (
@@ -25,7 +21,7 @@ func fronthandler(w http.ResponseWriter, r *http.Request) {
 
 	expires := time.Now().Add(time.Minute * 10)
 
-	// this will not work since impersonation is not used
+	// this will not work
 	// s, err := storage.SignedURL(bucketName, objectName, &storage.SignedURLOptions{
 	// 	Scheme:         storage.SigningSchemeV4,
 	// 	GoogleAccessID: serviceAccountName,
@@ -35,63 +31,63 @@ func fronthandler(w http.ResponseWriter, r *http.Request) {
 
 	// **********************************
 
-	//ctx := context.Background()
-	// storageClient, err := storage.NewClient(ctx)
+	// this will work
+	ctx := context.Background()
+	storageClient, err := storage.NewClient(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting client %s\n", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+
+	s, err := storageClient.Bucket(bucketName).SignedURL(objectName, &storage.SignedURLOptions{
+		Method:  http.MethodGet,
+		Expires: expires,
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting signedURL %s\n", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+
+	// this will also work but is not the right way to do this...
+	// ctx := context.Background()
+
+	// rootTokenSource, err := google.DefaultTokenSource(ctx, "https://www.googleapis.com/auth/iam")
 	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Error getting client %s\n", err)
 	// 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 	// }
+	// delegates := []string{}
 
-	// s, err := storageClient.Bucket(bucketName).SignedURL(objectName, &storage.SignedURLOptions{
+	// s, err := storage.SignedURL(bucketName, objectName, &storage.SignedURLOptions{
+	// 	Scheme:         storage.SigningSchemeV4,
+	// 	GoogleAccessID: serviceAccountName,
+	// 	SignBytes: func(b []byte) ([]byte, error) {
+	// 		client := oauth2.NewClient(context.TODO(), rootTokenSource)
+	// 		service, err := iamcredentials.New(client)
+	// 		if err != nil {
+	// 			return nil, fmt.Errorf("oauth2/google: Error creating IAMCredentials: %v", err)
+	// 		}
+	// 		signRequest := &iamcredentials.SignBlobRequest{
+	// 			Payload:   base64.StdEncoding.EncodeToString(b),
+	// 			Delegates: delegates,
+	// 		}
+	// 		name := fmt.Sprintf("projects/-/serviceAccounts/%s", serviceAccountName)
+	// 		at, err := service.Projects.ServiceAccounts.SignBlob(name, signRequest).Do()
+	// 		if err != nil {
+	// 			return nil, fmt.Errorf("oauth2/google: Error calling iamcredentials.SignBlob: %v", err)
+	// 		}
+	// 		sDec, err := base64.StdEncoding.DecodeString(at.SignedBlob)
+	// 		if err != nil {
+	// 			return nil, fmt.Errorf("oauth2/google: Error decoding iamcredentials.SignBlob response: %v", err)
+	// 		}
+	// 		return sDec, nil
+	// 	},
 	// 	Method:  http.MethodGet,
 	// 	Expires: expires,
 	// })
 	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Error getting signedURL %s\n", err)
+	// 	fmt.Printf("%v", err)
 	// 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 	// }
-
-	// this will work
-	ctx := context.Background()
-
-	rootTokenSource, err := google.DefaultTokenSource(ctx, "https://www.googleapis.com/auth/iam")
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-	}
-	delegates := []string{}
-
-	s, err := storage.SignedURL(bucketName, objectName, &storage.SignedURLOptions{
-		Scheme:         storage.SigningSchemeV4,
-		GoogleAccessID: serviceAccountName,
-		SignBytes: func(b []byte) ([]byte, error) {
-			client := oauth2.NewClient(context.TODO(), rootTokenSource)
-			service, err := iamcredentials.New(client)
-			if err != nil {
-				return nil, fmt.Errorf("oauth2/google: Error creating IAMCredentials: %v", err)
-			}
-			signRequest := &iamcredentials.SignBlobRequest{
-				Payload:   base64.StdEncoding.EncodeToString(b),
-				Delegates: delegates,
-			}
-			name := fmt.Sprintf("projects/-/serviceAccounts/%s", serviceAccountName)
-			at, err := service.Projects.ServiceAccounts.SignBlob(name, signRequest).Do()
-			if err != nil {
-				return nil, fmt.Errorf("oauth2/google: Error calling iamcredentials.SignBlob: %v", err)
-			}
-			sDec, err := base64.StdEncoding.DecodeString(at.SignedBlob)
-			if err != nil {
-				return nil, fmt.Errorf("oauth2/google: Error decoding iamcredentials.SignBlob response: %v", err)
-			}
-			return sDec, nil
-		},
-		Method:  http.MethodGet,
-		Expires: expires,
-	})
-
-	if err != nil {
-		fmt.Printf("%v", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-	}
 	fmt.Println(s)
 	w.Header().Set("context-type", "text/plain")
 	fmt.Fprint(w, s)
